d14907c140ac6ac04a8645c65763d8088d1f0255
Adithya S D <adithyasd@gmail.com>
2025-06-20 13:24:19 +0530

    Updated project with useState, useReducer, and useEffect hooks

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,6 @@
-import './App.css'
+import {useState,useReducer,useEffect} from "react";
+import './App.css';
+import messi from "./images/messi.jpg";
 
 function Header(props){
   console.log(props)
@@ -16,23 +18,54 @@ const items = [
   "Suarez"
 ];
 
-function Main({dishes}){
+function Main({dishes,openStatus,onStatus}){
   return(
+  <>
+    <div>
+       <button onClick={() => onStatus(true)}>
+        i wish you would be free
+        </button>
+      <h2>Welcome to beautiful Restuarant!{" "}
+        {openStatus ? "Free" : "Busy"}</h2>
+    </div>
     <ul>
       {dishes.map((dish)=>(
         <li style={{listStyleType:"none"}}>{dish}</li>
-      ))}
+      ))}react
     </ul>
+  </>
+    
   );
 }
 
 function App(){
+  const [status,toggle] = useReducer(
+    (status)=> !status,
+    true
+  )
+
+  useEffect(()=>{console.log(
+      `I am ${status ? "Free" : "Busy" }`
+    );});
+  
   return(
-    <div>
-    <Header name="Surya" year={2007}/>
-    <Main dishes={items} />
-    <h2>This is my first app</h2>
-  </div>
+    <>
+      <div>
+        <button onClick={toggle}>
+          {status ? "Busy" : "Free"} Me
+        </button>
+        <h1>Currently i am {" "}
+          {status ? "Free" : "Busy"}!
+        </h1>
+      </div> 
+        <Header name="Surya" year={2007}/>
+        <img src={messi} height={200}/>
+        <Main dishes={items}
+          openStatus={status}
+          onStatus={toggle}
+        />
+        <h2>This is my first app</h2>
+    </>
   );
 }
 
